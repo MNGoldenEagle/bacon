4f27216eaf5fdebb088cd6af738fa8e12a20f2d7
MNGoldenEagle <17274702+MNGoldenEagle@users.noreply.github.com>
2022-10-07 16:45:50 -0500

    Working and in decent shape

--- bacon.ts
+++ bacon.ts
@@ -9,7 +9,7 @@ const WIKI_RELATIVE_PREFIX: string = '/wiki/';
 const TARGET_TOPIC: string = 'Kevin_Bacon';
 const EXPLORED_TOPICS: Set<string> = new Set();
 const LOADED_TOPICS: Set<string> = new Set();
-const ABORT = new AbortController();
+let DEBUG: boolean = false;
 
 function cleanupTopic(topic: string) {
     let cleaned = topic.replace(" ", "_");
@@ -33,7 +33,6 @@ async function getLinksToTopic(topic: string): Promise<string[]> {
             Accept: "text/html"
         },
         responseType: "text",
-        signal: ABORT.signal,
         validateStatus: (_status) => true
     });
 
@@ -66,51 +65,68 @@ async function getLinksToTopic(topic: string): Promise<string[]> {
     return Array.from(result);
 }
 
-async function desperatelySeekingBacon(topic: string, depth: number): Promise<number> {
-    if (topic === TARGET_TOPIC) {
+async function desperatelySeekingBacon(topics: string[], depth: number): Promise<number> {
+    if (topics.includes(TARGET_TOPIC)) {
         return depth;
     }
 
-    const links = await getLinksToTopic(topic);
-    const newLinks = links.filter(link => !EXPLORED_TOPICS.has(link));
-    newLinks.forEach(link => EXPLORED_TOPICS.add(link));
+    // Split the list of topics into reasonably sized chunks and get the lists of links for every topic at this depth.
+    if (DEBUG) console.debug(`Scanning ${topics.length} topics at depth ${depth}.`)
 
-    // Dead end check.  If we have run out of links somehow, reject the promise.
+    let links: string[] = [];
+    let newLinks: string[] = [];
+
+    for (let i = 0; i < topics.length; i += 20) {
+        const topicSlice = topics.slice(i, i + 20);
+        if (DEBUG) console.debug(`Processing ${i + 1}/${topics.length}...`);
+        const sliceResults = await Promise.allSettled(topicSlice.map(topic => getLinksToTopic(topic)));
+        const sliceLinks = sliceResults.filter(result => result.status === 'fulfilled')
+            .map(result => result as PromiseFulfilledResult<string[]>)
+            .map(result => result.value)
+            .flat();
+        links = links.concat(sliceLinks);
+        const sliceNewLinks = sliceLinks.filter(link => !EXPLORED_TOPICS.has(link));
+        sliceNewLinks.forEach(link => EXPLORED_TOPICS.add(link));
+        newLinks = newLinks.concat(sliceNewLinks)
+
+        // Avoid having to load additional web pages if we can already determine that our target is included in the
+        // list of topics.
+        if (sliceNewLinks.includes(TARGET_TOPIC)) {
+            return depth + 1;
+        }
+    }
+
+    if (DEBUG) console.debug(`Found ${links.length} links at this depth. Of those, ${newLinks.length} are new.`);
+
+    // Dead end check.  If we have run out of links, throw an error.
     if (newLinks.length === 0) {
-        throw new Error(`Could not find ${TARGET_TOPIC}, current topic is ${topic} at depth ${depth}`);
+        throw new Error(`Could not find ${TARGET_TOPIC} after checking at a depth of ${depth}.`);
     }
 
-    // Avoid having to load additional web pages if we can already determine that our target is included in the
-    // list of topics.
-    if (newLinks.includes(TARGET_TOPIC)) {
-        return depth + 1;
-    }
-
-    if (ABORT.signal.aborted) {
-        throw new Error("Aborted");
-    }
-
-    return Promise.race(newLinks.map(value => desperatelySeekingBacon(value, depth + 1)));
+    return desperatelySeekingBacon(newLinks, depth + 1);
 }
 
 const cmd = program.command("bacon")
     .description("Goes wiki-diving from the topic of your choice in search of Bacon.  Kevin Bacon.")
     .argument("<topic>", "Wikipedia topic to start diving from. Can be either a URL or topic string (case sensitive).", cleanupTopic)
     .option("-d --debug", "Displays debugging information upon completion.")
+    .option("-t --trail", "Show the resulting trail of topics leading to the Bacon number.")
     .version(pkg.version)
     .parse();
 
 const args: string[] = cmd.processedArgs;
 let topic = args[0]
+DEBUG = cmd.opts()["debug"]
 
-console.log(`Starting from ${topic}.`);
+if (DEBUG) console.log(`Starting from ${topic}.`);
 
-desperatelySeekingBacon(topic, 0)
+desperatelySeekingBacon([ topic ], 0)
     .then(depth => console.log(`The Bacon Number for ${topic} is ${depth}.`))
     .catch(error => console.error(`Failed to get Bacon Number: ${error}`))
     .finally(() => {
-        console.log(`Explored topics size is ${EXPLORED_TOPICS.size}.`);
-        console.log(`Loaded topics are ${Array.from(LOADED_TOPICS)}.`);
-        ABORT.abort();
+        if (DEBUG) {
+            console.log(`Explored topics size is ${EXPLORED_TOPICS.size}.`);
+            console.log(`Loaded topics size is ${LOADED_TOPICS.size}.`);
+        }
         return;
     });
